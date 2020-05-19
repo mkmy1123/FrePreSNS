1a82af4183350d84c387274f53e200a33439f226
mkmy1123 <mkmy1123@gmail.com>
2020-05-19 04:44:10 +0000

    ADD views to Design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,17 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
+
+$(function () {
+
+  $('.menu-trigger').on('click', function () {
+    $(this).toggleClass('active');
+    $('#menu').fadeToggle();
+    return false;
+  });
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get '/' => 'home#top' 
+  root 'home#top' 
   get 'about' => 'home#about'
   get 'howto' => 'home#howto'
 
