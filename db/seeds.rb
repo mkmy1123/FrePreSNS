4d405cc5fffd8b87e4cb583f648584758f08e047
mkmy1123 <mkmy1123@gmail.com>
2020-06-02 04:34:17 +0000

    Finish search funcrion :debug completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  [
+    {
+      email: 'test1@test.com',
+      optional_id: 'yamadamada',
+      name: 'やまだ',
+      password: 'testtest',
+      password_confirmation: 'testtest'
+    },
+    {
+      email: 'test2@test.com',
+      optional_id: 'yeswecando',
+      name: 'できないことなんてない',
+      password: 'testtest',
+      password_confirmation: 'testtest'
+    },
+    {
+      email: 'test3@test.com',
+      optional_id: 'heybabyboy',
+      name: 'こんにちは坊や',
+      password: 'testtest',
+      password_confirmation: 'testtest'
+    },
+    {
+      email: 'test4@test.com',
+      optional_id: 'goodgirl',
+      name: 'いいこちゃん',
+      password: 'testtest',
+      password_confirmation: 'testtest'
+    },
+    {
+      email: 'test5@test.com',
+      optional_id: 'helloworld',
+      name: '駆け出しエンジニア',
+      password: 'testtest',
+      password_confirmation: 'testtest'
+    }
+  ]
+)
+
+Argument.create!(
+  [
+    {
+      target: 4,
+      topic: "AWSは使えるようになるべきだ"
+    },
+    {
+      target: 1,
+      topic: "政治に興味を持つ必要がある"
+    }
+  ]
+)
