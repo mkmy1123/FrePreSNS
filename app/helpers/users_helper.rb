dd50415ca374e298fe3af58c09fca67c7992b418
mkmy1123 <mkmy1123@gmail.com>
2020-06-12 09:49:41 +0000

    Improve code for DRY around TRUST / DM

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,18 @@
 module UsersHelper
 
-  def avator?(user)
-    if @user.avatar? 
-      image_tag @user.avatar.to_s
+  def avatar?(user)
+    if user.avatar? 
+      image_tag user.avatar.to_s
     else
       image_tag asset_path("default.jpg"), :size => '100x100'
     end
   end
 
-  def half_avator?(user)
-    if @user.avatar? 
-      image_tag @user.avatar.thumb_half.to_s
+  def half_avatar?(user)
+    if user.avatar? 
+      image_tag user.avatar.thumb_half.to_s
     else
-      image_tag asset_path("default.jpg"), :size => '100x100'
+      image_tag asset_path("default.jpg"), :size => '50x50'
     end
   end
 
