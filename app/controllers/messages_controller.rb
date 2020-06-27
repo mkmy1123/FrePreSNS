1c11b345dcdc25436fb5c6e7c1c045f7cd9649fd
mkmy1123 <mkmy1123@gmail.com>
2020-06-27 08:58:12 +0000

    FINISH no.2 problem

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,6 @@ class MessagesController < ApplicationController
       @message =
         Message.create(params.require(:message).
         permit(:user_id, :content, :room_id).merge(user_id: current_user.id))
-      redirect_to "/rooms/#{@message.room_id}"
     else
       redirect_back(fallback_location: root_path)
     end
